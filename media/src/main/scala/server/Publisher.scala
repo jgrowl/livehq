357dbc261d4a0746974d4b5436510e5000d8f9c7
Jonathan Rowlands <jonrowlands83@gmail.com>
2014-11-23 00:05:23 -0500

    Missed a couple!

--- media/src/main/scala/server/Publisher.scala
+++ media/src/main/scala/server/Publisher.scala
@@ -3,36 +3,17 @@ package server
 import akka.actor._
 import akka.cluster.MemberStatus
 import akka.contrib.pattern.ShardRegion
-import akka.contrib.pattern.ShardRegion.Passivate
-import akka.persistence.PersistentActor
+import livehq._
 import org.webrtc.PeerConnection.{IceConnectionState, IceGatheringState, SignalingState}
 import org.webrtc._
-import server.Publisher._
-import tv.camfire.actor.Registry
-import tv.camfire.actor.Registry.Internal
 import tv.camfire.media.callback.Callback
 import tv.camfire.media.webrtc.WebRtcHelper
+
+import scala.collection.mutable
 
 object Publisher {
   def props(webrtcHelper: WebRtcHelper, callback: Callback): Props =
     Props(new Publisher(webrtcHelper, callback))
-
-  sealed trait Command {
-    def identifier: String
-  }
-
-  object Incoming {
-    sealed trait Incoming
-
-    case class AddPeerConnection(identifier: String) extends Command
-    case class Offer(identifier: String, sessionDescription: SessionDescription) extends Command with Incoming
-    case class Candidate(identifier: String, iceCandidate: IceCandidate) extends Command with Incoming
-    case class Subscribe(identifier: String, targetIdentifier: String) extends Command with Incoming
-  }
-
-  sealed trait Event
-  case class PeerConnectionsAdded(pcs: (PeerConnection, scala.collection.mutable.HashMap[String, PeerConnection])) extends Event
-  case class MediaStreamAdded(ms: MediaStream) extends Event
 
   val idExtractor: ShardRegion.IdExtractor = {
     case cmd: Command => (cmd.identifier, cmd)
@@ -43,130 +24,125 @@ object Publisher {
   }
 
   val shardName: String = "Publisher"
-
-  private case class State(peerConnection: PeerConnection, clusterPeerConnections: scala.collection.mutable.HashMap[String, PeerConnection], mediaStream: MediaStream) {
-
-    def updated(evt: Event): State = evt match {
-      case PeerConnectionsAdded(pcs) => copy(peerConnection = pcs._1, clusterPeerConnections = pcs._2)
-      case MediaStreamAdded(ms) => copy(mediaStream = ms)
-    }
-  }
 }
 
-class Publisher(webRtcHelper: WebRtcHelper, callback: Callback) extends PersistentActor with ActorLogging
-  with PeerConnection.Observer {
+class Publisher(webRtcHelper: WebRtcHelper, callback: Callback) extends Actor
+with ActorLogging
+with PeerConnection.Observer {
+  log.info(s"Creating PeerConnection.")
+  private val _incomingPeerConnection = new PcDetails("", webRtcHelper.createPeerConnection(this))
 
-  val cluster = akka.cluster.Cluster(context.system)
+  private val _registryPeerConnections = mutable.Map.empty[String, PcDetails]
 
-  def members: scala.collection.immutable.SortedSet[akka.cluster.Member] = {
-    cluster.state.members.filter(_.status == MemberStatus.Up)
-  }
-
-  // self.path.parent.name is the type name (utf-8 URL-encoded)
-  // self.path.name is the entry identifier (utf-8 URL-encoded)
-  override def persistenceId: String = self.path.parent.name + "-" + self.path.name
-
-  // passivate the entity when no activity
-//  context.setReceiveTimeout(2.minutes)
-
-  private var state = State(null, null, null)
-
-  private var _identifier: String = null
-
-  override def receiveRecover: Receive = {
-//        case evt: PeerConnectionAdded =>
-//          context.become(created)
-//          state = state.updated(evt)
-//    case evt: PostAdded =>
-//      context.become(created)
-//      state = state.updated(evt)
-//    case evt @ PostPublished =>
-//      context.become(created)
-//      state = state.updated(evt)
-//    case evt: Event => state =
-//      state.updated(evt)
-    case _ =>
-      log.error("Should not hit this yet")
-
-  }
-
-  override def receiveCommand: Receive = initial
-
-  def initial: Receive = {
-    case Incoming.AddPeerConnection(identifier: String) =>
-      log.info("Creating PeerConnection for [{}]", identifier)
-      _identifier = identifier
-
-      val clusterPeerConnections = createRegistryPeerConnections(identifier)
-      // Immediately do the offer/answer/candidate dance with all registry members
-      for ((path, clusterPeerConnection) <- clusterPeerConnections) {
-        val offer = webRtcHelper.createOffer(clusterPeerConnection)
-        // TODO: make sure actorSelection is what we actually want
-        context.system.actorSelection(path) ! Registry.Internal.Offer(path, offer)
-//        context.system.actorSelection(path) ! Registry.Internal.Offer(identifier, offer)
+  override def receive: Actor.Receive = {
+    case Incoming.Offer(identifier, sessionDescription) =>
+      log.info(s"Incoming.Offer received.")
+      val answer = webRtcHelper.createAnswer(_incomingPeerConnection.peerConnection, sessionDescription)
+      if (answer.isDefined) {
+        callback.sendAnswer(identifier, answer.get)
+      } else {
+        log.error("Failed to create answer! No answer will be sent back!")
       }
+    case Incoming.Candidate(identifier, iceCandidate) =>
+      log.info(s"Incoming.Candidate received.")
+      _incomingPeerConnection.peerConnection.addIceCandidate(iceCandidate)
 
-      val peerConnection = webRtcHelper.createPeerConnection(this)
+    case Incoming.Subscribe(identifier: String, targetIdentifier: String) =>
+      log.info(s"Incoming.Subscribe received ($identifier -> $targetIdentifier)")
+      context.system.actorSelection("user/registry") ! Incoming.Subscribe(identifier, targetIdentifier)
 
-      state = state.updated(PeerConnectionsAdded((peerConnection, clusterPeerConnections)))
-      context.become(created)
+    // Internal
+    case Internal.Candidate(identifier, uuid, candidate) =>
+      log.info(s"Internal.Candidate received ($uuid).")
+      _registryPeerConnections.get(uuid).get.peerConnection.addIceCandidate(candidate)
+    case Internal.Answer(identifier, uuid, answer) =>
+      log.info(s"Internal.Answer received ($uuid).")
+      webRtcHelper.setRemoteDescription(_registryPeerConnections.get(uuid).get.peerConnection, answer)
 
-//      persist(PeerConnectionAdded(pc)) { evt =>
-//        state = state.updated(evt)
-//        context.become(created)
-//        log.info("New PeerConnection created: {}", "id for pc")
-//      }
-    case _ =>
-      println("should not be here")
+    case Internal.CreateRegistryPeerConnections(identifier) =>
+      log.info(s"Internal.CreateRegistryPeerConnections received.")
+      //      // TODO: I figure if we get a CONNECTED, then we should assume we need to always reconnect to all registry members
+      //      // from scratch. Seems like we should just clear them out before doing anything. I need to evaluate whether this
+      //      // is a good idea.
+      //      _registryPeerConnections.clear()
+
+      _initializeRegistryPeerConnections(_identifier)
+
+    case Internal.AttachMediaStreams(identifier, uuid) =>
+      log.info(s"Internal.AttachMediaStreams received.")
+      val registryPcDetails = _registryPeerConnections.get(uuid).get
+      _incomingPeerConnection.attachStreams(registryPcDetails, webRtcHelper.createConstraints)
+      // Send out an updated offer after MediaStream(s) have been added.
+      val offer = webRtcHelper.createOffer(registryPcDetails.peerConnection)
+      if (offer.isDefined) {
+        context.system.actorSelection(registryPcDetails.path) ! Internal.Offer(identifier, uuid, offer.get)
+      } else {
+        log.error(s"($uuid) Failed to create an offer! No offer will be sent!")
+      }
   }
 
-  def createRegistryPeerConnections(identifier: String): scala.collection.mutable.HashMap[String, PeerConnection] = {
-    val clusterPeerConnections = scala.collection.mutable.HashMap.empty[String, PeerConnection]
+  override def onSignalingChange(signalState: SignalingState): Unit = {
+    log.info(s"onSignalingChange : [${signalState.name()}].")
+  }
 
-    for (path <- registryPaths()) {
-      log.info(s"Creating PeerConnection for registry at $path")
-      val registry = context.system.actorSelection(path)
+  override def onError(): Unit = {
+    log.error(s"onError.")
+  }
 
-        val registryObserver = new PeerConnection.Observer() {
-          override def onSignalingChange(signalState: SignalingState): Unit = {
-            log.info("onSignalingChange called: {}", signalState.name())
-          }
+  override def onIceCandidate(iceCandidate: IceCandidate): Unit = {
+    log.info(s"onIceCandidate.")
+    callback.sendIceCandidate(_identifier, iceCandidate)
+  }
 
-          override def onError(): Unit = {
-            log.error("onError called...")
-          }
+  override def onRemoveStream(mediaStream: MediaStream): Unit = {
+    log.info(s"onRemoveStream : [${mediaStream.label()}.")
+    //    callback.onRemoveStream(sessionId, mediaStream.label())
+  }
 
-          override def onIceCandidate(candidate: IceCandidate): Unit = {
-            log.info("onIceCandidate called...")
-//            registry ! Internal.Candidate(path, candidate)
-            registry ! Internal.Candidate(identifier, candidate)
-          }
+  override def onIceGatheringChange(gatheringState: IceGatheringState): Unit = {
+    log.info(s"onIceGatheringChange : [${gatheringState.name()}].")
+  }
 
-          override def onRemoveStream(p1: MediaStream): Unit = {
-            log.info("onRemoveStream called...")
-          }
-
-          override def onIceGatheringChange(gatheringState: IceGatheringState): Unit = {
-            log.info("onIceGatheringChange called: [%s]...".format(gatheringState.name()))
-          }
-
-          override def onIceConnectionChange(connectionState: IceConnectionState): Unit = {
-            log.info("onIceConnectionChange called: {}...", connectionState.name())
-          }
-
-          override def onDataChannel(p1: DataChannel): Unit = ???
-
-          override def onAddStream(mediaStream: MediaStream): Unit = {
-            log.error("onAddStream called but the registry should never do this!")
-          }
-        }
-
-      val pc = webRtcHelper.createPeerConnection(registryObserver)
-      clusterPeerConnections.put(path, pc)
+  override def onIceConnectionChange(iceConnectionState: IceConnectionState): Unit = {
+    log.info(s"onIceConnectionChange : [${iceConnectionState.name()}].")
+    if (iceConnectionState == IceConnectionState.CONNECTED) {
+      // We have the actor send a message to itself because it will fail here if we try to have a PeerConnection
+      // create an offer. Not sure exactly why this is the case but might be a threading issue since we are dealing
+      // with an actor.
+      self ! Internal.CreateRegistryPeerConnections(_identifier)
     }
-
-    clusterPeerConnections
+    //    if (IceConnectionState.DISCONNECTED.equals(iceConnectionState)) {
+    //      //      log.debug("Connection [%s] disconnected, un-registering media streams...".format(sessionId))
+    //      //      callback.removeStreamsFromSession(sessionId)
+    //    }
   }
+
+  override def onAddStream(mediaStream: MediaStream): Unit = {
+    log.info(s"onAddStream : [${mediaStream.label()}]")
+
+    val duplicatedMediaStream = webRtcHelper.createDuplicateMediaStream(mediaStream, _identifier)
+
+    log.info(s"Adding MediaStream(${mediaStream.label()}) to [${_registryPeerConnections.size}] registry PeerConnection(s)")
+    _registryPeerConnections.foreach {
+      case (uuid, pcDetail) => {
+        if (pcDetail.peerConnection.addStream(duplicatedMediaStream, webRtcHelper.createConstraints)) {
+          log.info(s"($uuid) added duplicated stream : ${duplicatedMediaStream.label}")
+
+          val offer = webRtcHelper.createOffer(pcDetail.peerConnection)
+          if (offer.isDefined) {
+            context.system.actorSelection(pcDetail.path) ! offer.get
+          } else {
+            log.error(s"($uuid) Failed to create offer! No offer will be sent!")
+          }
+        } else {
+          log.error(s"($uuid) could not add duplicated stream : ${duplicatedMediaStream.label}")
+        }
+      }
+    }
+    //    callback.onAddStream(_identifier, mediaStream.label())
+  }
+
+  override def onDataChannel(p1: DataChannel): Unit = ???
 
   def registryPaths(): List[String] = {
     members.map(x => registryPath(x.address)).toList
@@ -176,82 +152,73 @@ class Publisher(webRtcHelper: WebRtcHelper, callback: Callback) extends Persiste
     s"${address.protocol}://${address.system}@${address.host.get}:${address.port.get}/user/registry"
   }
 
-  def created: Receive = {
-    case Incoming.Offer(identifier, sessionDescription) =>
-      log.info("Establishing peerConnection for [{}]...", identifier)
-      callback.sendAnswer(identifier, webRtcHelper.createAnswer(state.peerConnection, sessionDescription))
-    case Incoming.Candidate(identifier, iceCandidate) =>
-      log.info("Adding iceCandidate to [{}]...", identifier)
-      state.peerConnection.addIceCandidate(iceCandidate)
+  val cluster = akka.cluster.Cluster(context.system)
 
-    case Incoming.Subscribe(identifier: String, targetIdentifier: String) =>
-      log.info("wat2")
-      log.info("Telling registry to subscribe [{}] to identifier: [{}]", identifier, targetIdentifier)
-      context.system.actorSelection("user/registry") ! Incoming.Subscribe(identifier, targetIdentifier)
-
-    // Internal
-    case Internal.Candidate(identifier, candidate) =>
-      state.clusterPeerConnections.get(identifier).get.addIceCandidate(candidate)
-    case Internal.Answer(identifier, answer) =>
-      webRtcHelper.setRemoteDescription(state.clusterPeerConnections.get(identifier).get, answer)
-    case _ =>
-      print("herptidyderpty");
+  def members: scala.collection.immutable.SortedSet[akka.cluster.Member] = {
+    cluster.state.members.filter(_.status == MemberStatus.Up)
   }
 
-  override def unhandled(msg: Any): Unit = msg match {
-    case ReceiveTimeout => context.parent ! Passivate(stopMessage = PoisonPill)
-    case _              => super.unhandled(msg)
-  }
+  // TODO: Not sure if this will always work
+  def _identifier = self.path.name
 
-  override def onSignalingChange(p1: SignalingState): Unit = {
-    log.info("onSignalingChange called: {}", p1.name())
-  }
+  def newUuid = java.util.UUID.randomUUID.toString
 
-  override def onError(): Unit = {
-    log.error("onError called...")
-  }
+  def _initializeRegistryPeerConnections(identifier: String): Unit = {
+    for (path: String <- registryPaths()) {
+      val uuid = newUuid
+      log.info(s"Creating registry PeerConnection($uuid) for [$path]")
+      val registry = context.system.actorSelection(path)
 
-  override def onIceCandidate(iceCandidate: IceCandidate): Unit = {
-    log.info("onIceCandidate called...")
-    callback.sendIceCandidate(_identifier, iceCandidate)
-  }
+      val registryObserver = new PeerConnection.Observer() {
+        override def onSignalingChange(signalState: SignalingState): Unit = {
+          log.info(s"RegistryPeerConnection($uuid).onSignalingChange : [${signalState.name()}].")
+        }
 
-  override def onRemoveStream(p1: MediaStream): Unit = {
-    log.info("onRemoveStream called...")
-    //    callback.onRemoveStream(sessionId, mediaStream.label())
-  }
+        override def onError(): Unit = {
+          log.error(s"RegistryPeerConnection($uuid).onError!")
+        }
 
-  override def onIceGatheringChange(p1: IceGatheringState): Unit = {
-    log.info("onIceGatheringChange called: [%s]...".format(p1.name()))
-  }
+        override def onIceCandidate(candidate: IceCandidate): Unit = {
+          log.info(s"RegistryPeerConnection($uuid).onIceCandidate [${candidate.toString}]")
+          registry ! Internal.Candidate(identifier, uuid, candidate)
+        }
 
-  override def onIceConnectionChange(p1: IceConnectionState): Unit = {
-    log.info("onIceConnectionChange called: {}...", p1.name())
-    if (IceConnectionState.DISCONNECTED.equals(p1)) {
-      //      log.debug("Connection [%s] disconnected, un-registering media streams...".format(sessionId))
-      //      callback.removeStreamsFromSession(sessionId)
-    }
-  }
+        override def onRemoveStream(mediaStream: MediaStream): Unit = {
+          log.info(s"RegistryPeerConnection($uuid).onRemoveStream : [${mediaStream.label()}.")
+        }
 
-  override def onAddStream(mediaStream: MediaStream): Unit = {
-    log.info("onAddStream called, adding mediaStream [{}]", mediaStream.label())
-    state = state.updated(MediaStreamAdded(mediaStream))
-    val duplicatedMediaStream = webRtcHelper.createDuplicateMediaStream(mediaStream, _identifier)
+        override def onIceGatheringChange(gatheringState: IceGatheringState): Unit = {
+          log.info(s"RegistryPeerConnection($uuid).onIceGatheringChange : [${gatheringState.name()}].")
+        }
 
-    for (clusterPeerConnection <- state.clusterPeerConnections) {
-      if (clusterPeerConnection._2.addStream(duplicatedMediaStream, webRtcHelper.createConstraints)) {
-        log.info("Added duplicated stream [{}] to [{}]", duplicatedMediaStream.label, clusterPeerConnection._1)
+        override def onIceConnectionChange(iceConnectionState: IceConnectionState): Unit = {
+          log.info(s"RegistryPeerConnection($uuid).onIceConnectionChange : [${iceConnectionState.name()}].")
+          if (iceConnectionState == IceConnectionState.CONNECTED) {
+            // Now that we're connected to the registry PeerConnection, we can add any MediaStreams
+            log.info(s"RegistryPeerConnection($uuid) Attaching MediaStreams.")
+
+            self ! Internal.AttachMediaStreams(identifier, uuid)
+          }
+        }
+
+        override def onAddStream(mediaStream: MediaStream): Unit = {
+          log.info(s"RegistryPeerConnection($uuid).onAddStream : [${mediaStream.label()}]")
+          log.error("onAddStream called but the registry should never do this!")
+        }
+
+        override def onDataChannel(p1: DataChannel): Unit = ???
+      }
+
+      val pc = webRtcHelper.createPeerConnection(registryObserver)
+      _registryPeerConnections.put(uuid, new PcDetails(path, pc))
+
+      // Do the offer/answer/candidate dance with all registry members
+      val offer = webRtcHelper.createOffer(pc)
+      if (offer.isDefined) {
+        context.system.actorSelection(path) ! Internal.Offer(identifier, uuid, offer.get)
       } else {
-        log.error("Could not add duplicated stream to [{}]", clusterPeerConnection._1)
+        log.error(s"($uuid) Failed to create an offer! No offer will be sent!")
       }
     }
-
-    // Add MediaStream to local registry
-    context.system.actorSelection("user/registry") ! Registry.Internal.AddMediaStream(_identifier, duplicatedMediaStream)
-
-    callback.onAddStream(_identifier, mediaStream.label())
   }
-
-  override def onDataChannel(p1: DataChannel): Unit = ???
-
 }
--- media/src/main/scala/server/WebRtcApp.scala
+++ media/src/main/scala/server/WebRtcApp.scala
@@ -1,14 +1,9 @@
 package server
 
-import akka.actor.{ActorIdentity, ActorPath, ActorSystem, Identify, Props}
+import akka.actor.Props
 import akka.contrib.pattern.ClusterSharding
-import akka.pattern.ask
-import akka.persistence.journal.leveldb.{SharedLeveldbJournal, SharedLeveldbStore}
-import akka.util.Timeout
-import com.typesafe.config.ConfigFactory
+import tv.camfire.actor.Registry
 import tv.camfire.media.config.LogicModule
-
-import scala.concurrent.duration._
 
 object WebRtcApp {
   def main(args: Array[String]): Unit = {
@@ -29,8 +24,7 @@ object WebRtcApp {
 
       val system = modules.actorSystem
 
-      startupSharedJournal(system, startStore = (_port == "2551"), path =
-        ActorPath.fromString("akka.tcp://ClusterSystem@127.0.0.1:2551/user/store"))
+      val registry = system.actorOf(Props(new Registry(modules.webRtcHelper, modules.callback)), "registry")
 
       val connectionRegion = ClusterSharding(system).start(
         typeName = Publisher.shardName,
@@ -38,41 +32,10 @@ object WebRtcApp {
         idExtractor = Publisher.idExtractor,
         shardResolver = Publisher.shardResolver)
 
-//      ClusterSharding(system).start(
-//        typeName = Post.shardName,
-//        entryProps = Some(Post.props(authorListingRegion)),
-//        idExtractor = Post.idExtractor,
-//        shardResolver = Post.shardResolver)
-
-
 //      if (port != "2551" && port != "2552") {
 //        system.actorOf(Props[Bot], "bot")
 //      }
-
-    }
-
-    def startupSharedJournal(system: ActorSystem, startStore: Boolean, path: ActorPath): Unit = {
-      // Start the shared journal one one node (don't crash this SPOF)
-      // This will not be needed with a distributed journal
-      if (startStore)
-        system.actorOf(Props[SharedLeveldbStore], "store")
-      // register the shared journal
-      import system.dispatcher
-      implicit val timeout = Timeout(15.seconds)
-      val f = (system.actorSelection(path) ? Identify(None))
-      f.onSuccess {
-        case ActorIdentity(_, Some(ref)) => SharedLeveldbJournal.setStore(ref, system)
-        case _ =>
-          system.log.error("Shared journal not started at {}", path)
-          system.shutdown()
-      }
-      f.onFailure {
-        case _ =>
-          system.log.error("Lookup of shared journal at {} timed out", path)
-          system.shutdown()
-      }
     }
   }
-
 }
 
