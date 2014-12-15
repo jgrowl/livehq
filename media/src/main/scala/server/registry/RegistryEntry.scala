f009fe82055bc7909dda4c070a76307a5c1a68da
Jonathan Rowlands <jonrowlands83@gmail.com>
2014-12-14 23:53:46 -0500

    Added (missing) RegistryEntry.

---
+++ media/src/main/scala/server/registry/RegistryEntry.scala
@@ -0,0 +1,92 @@
+package server.registry
+
+import akka.actor.{Actor, ActorLogging, ActorRef}
+import livehq._
+import server.Utils
+import server.pc.obs.RegistryToPublisherPcObs
+import tv.camfire.media.callback.Callback
+import tv.camfire.media.webrtc.WebRtcHelper
+
+import scala.collection.mutable
+
+
+/**
+ * Created by jonathan on 12/12/14.
+ */
+class RegistryEntry(webRtcHelper: WebRtcHelper, callback: Callback, _identifier: String, publisher: ActorRef)
+  extends Actor with ActorLogging {
+  private val _uuid = Utils.uuid
+  private val _pcDetails: PcDetails = _initPcDetails()
+  publisher ! Internal.RequestPeerConnection(_identifier, _uuid)
+  private val _pendingSubscribers = mutable.Set.empty[(String, ActorRef)]
+  private var _connected = false
+
+  override def receive: Receive = {
+    case Incoming.Subscribe(identifier: String, publisherIdentifier: String) =>
+      log.info(s"($identifier) subscribing to $publisherIdentifier.")
+      _addSubscriber(identifier, sender())
+    case Internal.Offer(identifier, uuid, offer) =>
+      val logId = Log.registrySubPcId(identifier, uuid)
+      log.info(s"$logId offer received.")
+      val answer = webRtcHelper.createAnswer(_pcDetails.peerConnection, offer)
+      if (answer.isDefined) {
+        sender ! Internal.Answer(identifier, uuid, answer.get)
+      } else {
+        log.error("Failed to create answer! No answer will be sent back for offer!")
+      }
+    case Internal.Candidate(identifier, uuid, candidate) =>
+      val logId = Log.registrySubPcId(identifier, uuid)
+      log.info(s"$logId candidate received.")
+      _pcDetails.peerConnection.addIceCandidate(candidate)
+    case Internal.Registry.AddStream(identifier, mediaStream) =>
+      val duplicatedMediaStream = webRtcHelper.createDuplicateMediaStream(mediaStream, identifier)
+      _pcDetails.addStream(duplicatedMediaStream)
+
+    //    // TODO: Add the ability to subscribe to a single label
+    case Internal.Registry.Connected(identifier) =>
+      _connected = true
+      _processPendingSubscribers()
+  }
+
+  private def _initPcDetails(): PcDetails = {
+    val logId = Log.registrySubPcId(_identifier, _uuid)
+    val observer = new RegistryToPublisherPcObs(
+      log,
+      logId,
+      webRtcHelper,
+      self,
+      publisher,
+      callback,
+      _identifier,
+      _uuid)
+
+    log.info(s"Creating $logId.")
+    callback.onRegistrySubInitialize(_identifier, _uuid, self.path.toString)
+    new StandardPcDetails(webRtcHelper.createPeerConnection(observer), webRtcHelper)
+  }
+
+  private def _addSubscriber(identifier: String, sender: ActorRef): Unit = {
+    if (_connected) {
+      _subscribe(identifier, sender)
+    } else {
+      _pendingSubscribers += identifier -> sender
+    }
+  }
+
+  private def _subscribe(identifier: String, subscriber: ActorRef): Unit = {
+    _pcDetails.getMediaStreams.foreach {
+      case (mediaStreamId, mediaStream) => {
+        log.info(s"Attempting to add $mediaStreamId to $identifier (${mediaStream.label()})...")
+        callback.onSubscribe(identifier, _identifier, mediaStream.label())
+        subscriber ! Internal.AddRegistryMediaStream(mediaStreamId, mediaStream)
+      }
+    }
+  }
+
+  private def _processPendingSubscribers(): Unit = {
+    _pendingSubscribers.foreach {
+      case (identifier, subscriber) =>
+        _subscribe(identifier, subscriber)
+    }
+  }
+}
