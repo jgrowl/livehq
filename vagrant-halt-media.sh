e71577dde7f263976f7ef3c0cb92f4bbf4ecd067
Jonathan Rowlands <jonrowlands83@gmail.com>
2015-06-10 23:30:31 -0400

    Removes old scripts.

--- vagrant-halt-media.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -eux
-vagrant halt publisher publisher-monitor subscriber subscriber-monitor
--- vagrant-up-media.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -eux
-
-vagrant up publisher publisher-monitor subscriber subscriber-monitor
